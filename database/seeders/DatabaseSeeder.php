d23c252ae51e89da8f6c56e6e259a78970b5949c
nelsonomoi <omoinelson3@gmail.com>
2021-06-10 22:10:10 +0300

    matrix multiplication added

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,15 +4,18 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 
+use DB;
+
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
+
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+        DB::table('users')->insert([
+            'name' => 'Tester',
+            'email' => 'tester@gmail.com',
+            'password' => bcrypt('abcd1234'),
+        ]);
     }
 }
