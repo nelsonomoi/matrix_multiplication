b839de8678e9a1e1614a9d320329dd0bf8133cd9
nelsonomoi <omoinelson3@gmail.com>
2021-06-12 16:58:21 +0300

    task completed

--- app/Classes/Matrixmultiplier.php
+++ app/Classes/Matrixmultiplier.php
@@ -17,7 +17,7 @@ class Matrixmultiplier
         $this->secondMatrix = $b;
     }
 
-    public function mult_matrix(): array
+    public function mult_matrix()
     {
         $row1 = count($this->firstMatrix);
 
@@ -28,13 +28,6 @@ class Matrixmultiplier
 
 //        return $prod;
 
-
-
-
-
-
-
-
         //Performs product of matrices a and b. Store the result in matrix prod
         for ($i = 0; $i < $row1; $i++) {
             for ($j = 0; $j < $col2; $j++) {
@@ -43,6 +36,47 @@ class Matrixmultiplier
                 }
             }
         }
+
+
+        for ($i = 0; $i < count($prod); $i++){
+            for ($j = 0 ; $j < count($prod[$i]);$j++){
+                $prod[$i][$j] = $this->encryptMatrixResult($prod[$i][$j]);
+            }
+        }
         return $prod;
+    }
+
+    public function encryptMatrixResult($value)
+    {
+        $alphabets = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+
+//        return $alphabets[0];
+        if ($value > 0){
+            if ($value >= 26){
+
+                $letter = (string)$alphabets[0];
+
+                $times = ($value - ($value % 26)) / 26 ;
+
+                $str = str_repeat($letter,$times);
+
+                if ($value % 26 == 0){
+                    $letter = $alphabets[25];
+                    $str = str_repeat((string)$letter,$times);
+                }else{
+                    $reminder = $value % 26;
+                    $str = $str.$alphabets[$reminder];
+                }
+
+                return $str;
+
+            }else{
+                return (string)$alphabets[$value];
+            }
+        }else{
+            return $value;
+        }
+
+
     }
 }
